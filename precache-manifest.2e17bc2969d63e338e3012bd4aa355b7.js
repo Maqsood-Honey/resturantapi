4ad2e74ada7b6f76cb751a9b7ad2a2c7f0cd6875
Maqsood Ahmad <maqsoodmanzoor@gmail.com>
2023-07-20 12:06:01 +0500

    Updates

---
+++ precache-manifest.2e17bc2969d63e338e3012bd4aa355b7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "7851a15f56a768719fa5",
+    "url": "/textmani/static/css/main.73e5e065.chunk.css"
+  },
+  {
+    "revision": "7851a15f56a768719fa5",
+    "url": "/textmani/static/js/main.7851a15f.chunk.js"
+  },
+  {
+    "revision": "27fd2703407bce1ff247",
+    "url": "/textmani/static/css/1.f40ae759.chunk.css"
+  },
+  {
+    "revision": "27fd2703407bce1ff247",
+    "url": "/textmani/static/js/1.27fd2703.chunk.js"
+  },
+  {
+    "revision": "eae4aa3ff6c841b88c08",
+    "url": "/textmani/static/js/runtime~main.eae4aa3f.js"
+  },
+  {
+    "revision": "055bdc75f3586ea0324d119aef6b5ad5",
+    "url": "/textmani/index.html"
+  }
+];
